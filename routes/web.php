1bfb49852154efde223fc2c27b4b178114e774f0
localgit <localgit@gmail.com>
2024-09-04 04:04:37 +0500

    laravel reverb (Broad casting Services)

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Events\testingEvent;
 
 Route::get('/', function () {
     return view('welcome'); 
+}); 
+
+
+Route::get('test', function () {
+
+    event(new testingEvent('hello from test'));
+
+    return 'done'; 
+
 }); 
